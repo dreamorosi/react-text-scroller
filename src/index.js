fd7fad6f857ca34c15dc5059681c8ce7a4926bb3
Andrea Amorosi <dreamorosi@gmail.com>
2019-04-14 23:09:20 +0200

    Changed to class

--- src/index.js
+++ src/index.js
@@ -6,32 +6,50 @@ const duration = 500;
 
 const defaultStyle = {
   transition: `transform ${duration}ms ease-in-out`,
-  transform: `translate(0px, 100px)`
+  transform: `translate(0px, 100px)`,
+  textAlign: "center",
+  height: "100%",
+  border: "1px solid green"
 };
 
 const transitionStyles = {
-  entering: { transition: "none" },
+  entering: { display: "block" },
   entered: { transform: `translate(0px, 40px)` },
-  exiting: { transform: `translate(0px, -20px)` },
-  exited: { transform: `translate(0px, -20px)`, display: "none" }
+  exiting: { transform: `translate(0px, -75px)` },
+  exited: { transform: `translate(0px, 100px)`, display: "none" }
 };
 
-const Fade = () => {
-  const [isOneVisible, setOneVisible] = useState(true);
-  const [isTwoVisible, setTwoVisible] = useState(false);
-  const [isThreeVisible, setThreeVisible] = useState(false);
-  return [
-    <div
-      style={{
-        border: "1px solid #000",
-        width: "150px",
-        height: "100px",
-        margin: "50px 50px",
-        overflow: "hidden"
-      }}
-      key={0}
-    >
-      <Transition in={isOneVisible} timeout={duration}>
+class TextScroller extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visibility: []
+    };
+
+    this.state.visibility = props.strings.map(() => false);
+
+    this.renderSlides = this.renderSlides.bind(this);
+    this.renderButtons = this.renderButtons.bind(this);
+    this.changeSlide = this.changeSlide.bind(this);
+  }
+
+  componentDidMount() {
+    const { strings } = this.props;
+
+    let visibility = [];
+    for (let i in strings) {
+      visibility.push(parseInt(i, 10) === 0);
+    }
+    this.setState({ visibility: visibility });
+  }
+
+  renderSlides() {
+    const { strings } = this.props;
+    const { visibility } = this.state;
+
+    return strings.map((el, i) => (
+      <Transition key={`${el}${i}`} in={visibility[i]} timeout={duration}>
         {state => (
           <div
             style={{
@@ -39,67 +57,81 @@ const Fade = () => {
               ...transitionStyles[state]
             }}
           >
-            Title 1
+            {el}
           </div>
         )}
       </Transition>
-      <Transition in={isTwoVisible} timeout={duration}>
-        {state => (
-          <div
-            style={{
-              ...defaultStyle,
-              ...transitionStyles[state]
-            }}
-          >
-            Title 2
-          </div>
-        )}
-      </Transition>
-      <Transition in={isThreeVisible} timeout={duration}>
-        {state => (
-          <div
-            style={{
-              ...defaultStyle,
-              ...transitionStyles[state]
-            }}
-          >
-            Title 3
-          </div>
-        )}
-      </Transition>
-    </div>,
-    <button
-      key={1}
-      onClick={() => {
-        setOneVisible(!isOneVisible);
-        setTwoVisible(false);
-        setThreeVisible(false);
-      }}
-    >
-      1
-    </button>,
-    <button
-      key={2}
-      onClick={() => {
-        setOneVisible(false);
-        setTwoVisible(!isTwoVisible);
-        setThreeVisible(false);
-      }}
-    >
-      2
-    </button>,
-    <button
-      key={3}
-      onClick={() => {
-        setOneVisible(false);
-        setTwoVisible(false);
-        setThreeVisible(!isThreeVisible);
-      }}
-    >
-      3
-    </button>
-  ];
+    ));
+  }
+
+  renderButtons() {
+    const { strings } = this.props;
+
+    return strings.map((el, i) => (
+      <button
+        key={`${el}${i}`}
+        onClick={() => {
+          this.changeSlide(i);
+        }}
+        style={{
+          alignSelf: "center"
+        }}
+      />
+    ));
+  }
+
+  changeSlide(idx) {
+    const { visibility } = this.state;
+
+    visibility.forEach((el, i) => {
+      if (i === idx && el) {
+        return;
+      }
+      visibility[i] = i === idx;
+      this.setState({ visibility: visibility });
+    });
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          border: "1px solid #000",
+          width: "inherit",
+          height: "inherit"
+        }}
+      >
+        <div
+          style={{
+            width: "inherit",
+            height: "80%",
+            overflow: "hidden"
+          }}
+          key={0}
+        >
+          {this.renderSlides()}
+        </div>
+        <div
+          style={{
+            width: "inherit",
+            height: "20%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          {this.renderButtons()}
+        </div>
+      </div>
+    );
+  }
+}
+
+const App = () => {
+  return (
+    <TextScroller strings={["Title 1", "Title 2", "Title 3", "Title 4"]} />
+  );
 };
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Fade />, rootElement);
+ReactDOM.render(<App />, rootElement);
